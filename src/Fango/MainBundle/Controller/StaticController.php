477366e49816e5e3213e6b926e05dcf9574f44ca
Farhad Safarov <farhad.safarov@gmail.com>
2015-06-03 10:27:44 +0300

    privacy page

---
+++ src/Fango/MainBundle/Controller/StaticController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Fango\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class StaticController
+ * @author Farhad Safarov <http://ferhad.in>
+ * @package Fango\MainBundle\Controller
+ */
+class StaticController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function privacyAction()
+    {
+        return $this->render('@FangoMain/Static/privacy.html.twig');
+    }
+}
